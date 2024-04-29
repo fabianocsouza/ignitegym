46b5ef9d809399c9bef3cfae60640e764d43e998
Fabiano C. Souza <fabiano1403@hotmail.com>
2024-04-29 11:23:52 -0300

    select photo device

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,9 +1,12 @@
-import { Center, Heading, ScrollView, Skeleton, Text, VStack } from 'native-base';
+import { useState } from 'react';
+import * as FileSystem from 'expo-file-system';
+import { TouchableOpacity } from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
+import { Center, Heading, ScrollView, Skeleton, Text, VStack, useToast } from 'native-base';
 
 import { ScreenHeader } from '@components/ScreenHeader';
 import { UserPhoto } from '@components/UserPhoto';
-import { useState } from 'react';
-import { TouchableOpacity } from 'react-native';
+
 import { Input } from '@components/Input';
 import { Button } from '@components/Button';
 
@@ -11,6 +14,47 @@ const PHOTO_SIZE = 33;
 
 export  function Profile() {
   const [photoIsLoading, setPhotoIsLoading] = useState(false);
+  const [userPhoto, setUserPhoto] = useState('https://github.com/fabianocsouza.png');
+
+  const toast = useToast();
+
+  async function handleUserPhotoSelect(){
+    setPhotoIsLoading(true)
+   try {
+    const photoSelected = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      quality: 1,
+      aspect: [4,4],
+      allowsEditing: true,
+    });
+
+    if(photoSelected.canceled){
+      return;
+    }
+
+    if(photoSelected.assets[0].uri){
+      const photoInfo = await FileSystem.getInfoAsync(photoSelected.assets[0].uri);
+
+      if(photoInfo.exists && (photoInfo.size / 1024/ 1024) > 5 ){
+        
+       return toast.show({
+        title: "Essa imagem é muito grande. Escolha uma de até 5MB.",
+        placement: "top",
+        bgColor: "red.500"
+       })
+      }
+      
+      setUserPhoto(photoSelected.assets[0].uri);
+    }
+    
+   } catch (error) {
+    console.log(error);
+    
+   }finally{
+    setPhotoIsLoading(false);
+   }
+  }
+
   return (
     <VStack flex={1}>
       <ScrollView
@@ -28,14 +72,14 @@ export  function Profile() {
                 endColor="gray.400"
               />
             : <UserPhoto
-              source={{uri : 'https://github.com/fabianocsouza.png'}}
+              source={{uri : userPhoto}}
               alt="Imagem do usuário"
               size={PHOTO_SIZE}
               mr={4}
             />
           }
 
-          <TouchableOpacity>
+          <TouchableOpacity onPress={handleUserPhotoSelect}>
             <Text color="green.500" fontWeight="bold" fontSize="md" mt={2} mb={8}>
               Alterar foto
             </Text>
@@ -64,7 +108,7 @@ export  function Profile() {
 
           <Input
             bg="gray.600"
-            placeholder="Nova antiga"
+            placeholder="Nova senha"
             secureTextEntry
           />
 
